9b2719628bed3455f021f05f5ddaa7dc95586fb8
Training22github <venkatesh.slack@gmail.com>
2022-11-13 05:03:40 +0530

    Adding the project

---
+++ src/test/java/StepDefinition/TestRunner.java
@@ -0,0 +1,35 @@
+package StepDefinition;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features="src//test//resources//Features",
+glue= {"StepDefinition","MyHooks"},
+plugin= {"pretty","html:target/htmlreports","json:target/jsonreports/jsonreports.json"},
+strict=true, monochrome = true)
+//tags = "@Smoke or @Regression")
+public class TestRunner extends AbstractTestNGCucumberTests{
+	@BeforeClass
+	public void first()
+	{
+		
+	}
+	@AfterClass
+	public void last()
+	{
+		
+	}
+}
+
+
+/*import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src\\test\\resources\\Features",
+glue= {"StepDefinition"},strict=true,monochrome=false,
+plugin= {"junit:target/junitreports/junitreports.xml"})
+public class TestRunner {
+}*/
